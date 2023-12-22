a610e76ed26247dff4e1a1cbc54fa86143725510
Parita Dave <72275690+paritadave@users.noreply.github.com>
2023-12-21 20:35:22 -0500

    Update Bert_qa.py

--- Bert_qa.py
+++ Bert_qa.py
@@ -2,11 +2,11 @@ import streamlit as st
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 # Load a suitable model from Hugging Face Transformers
-model_name = "facebook/bart-base"  # You can choose a different model if needed #xlm-roberta-base
+model_name = "facebook/bart-base"  # You can choose a different model if needed
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-st.title(" File Q&A with Hugging Face Transformers")
+st.title("File Q&A with Hugging Face Transformers")
 
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
 question = st.text_input(
@@ -15,13 +15,17 @@ question = st.text_input(
     disabled=not uploaded_file,
 )
 
-if uploaded_file and question:
-    article = uploaded_file.read().decode()
-    prompt = f"""Question: {question}\nContext: {article}"""
+try:
+    if uploaded_file and question:
+        article = uploaded_file.read().decode()
+        prompt = f"""Question: {question}\nContext: {article}"""
 
-    input_ids = tokenizer.encode(prompt, return_tensors="pt")
-    output = model.generate(input_ids)
-    answer = tokenizer.decode(output[0], skip_special_tokens=True)
+        input_ids = tokenizer.encode(prompt, return_tensors="pt")
+        output = model.generate(input_ids)
+        answer = tokenizer.decode(output[0], skip_special_tokens=True)
 
-    st.write("### Answer")
-    st.write(answer)
+        st.write("### Answer")
+        st.write(answer)
+except Exception as e:
+    st.write("An error occurred:", str(e))
+    # You can customize the error message as needed
